106d8702d826355cee8fb8be41eacbc84e8a6f8f
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2022-12-23 00:13:24 -0500

    Feature: light and floor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Canvas } from '@react-three/fiber'
 import { Sky } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import { Ground } from './components/Ground.jsx'
+import { FPV } from './components/FPV.jsx'
 
 function App () {
   return (
     <>
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5}/>
+        <FPV/>
+        <Physics>
+          <Ground/>
+        </Physics>
       </Canvas>
      
     </>
