5dddb12c9305cd714a0094cf9787f6ec7a4775d5
Johan794 <johan.ricardo1104@gmail.com>
2022-12-28 20:38:05 -0500

    feature: first person movement

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,15 @@
 import { useSphere } from "@react-three/cannon";
 import { useFrame, useThree } from "@react-three/fiber";
 import { Vector3 } from "three";
-import { useEffect, useRef } from 'react'
-import { useKeyboard } from '../hooks/useKeyboard.js'
+import { useEffect, useRef } from "react";
+import { useKeyboard } from "../hooks/useKeyboard.js";
 
+const CHARACTER_SPEED = 4;
+const CHARACTER_JUMP_FORCE = 4;
 export const Player = () => {
-    const actions = useKeyboard()
-    console.log(actions)
+  const { moveBackward, moveForward, moveLeft, moveRight, jump } =
+    useKeyboard();
 
-    
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
     mass: 1,
@@ -31,13 +32,43 @@ export const Player = () => {
   }, [api.velocity]);
   useFrame(() => {
     camera.position.copy(
-      new Vector3(
-        pos.current[0],
-        pos.current[1], 
-        pos.current[2] 
-      )
-    )
-    api.velocity.set(0,0,-1)
+      new Vector3(pos.current[0], pos.current[1], pos.current[2])
+    );
+
+    const direction = new Vector3();
+
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(CHARACTER_SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(
+      direction.x,
+      vel.current[1], // ???? saltar.
+      direction.z
+    );
+
+    if (jump && Math.abs(vel.current[1]) < 0.05) {
+        api.velocity.set(
+          vel.current[0],
+          CHARACTER_JUMP_FORCE,
+          vel.current[2]
+        )
+      }
+    //api.velocity.set(0, 0, -1);
   });
 
   return <mesh ref={ref} />;
