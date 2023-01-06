a071cdaa76544892dbc7858ee23612bed8b81c2c
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2023-01-05 21:48:18 -0500

    End

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,75 +1,87 @@
-import { useSphere } from "@react-three/cannon";
-import { useFrame, useThree } from "@react-three/fiber";
-import { Vector3 } from "three";
-import { useEffect, useRef } from "react";
-import { useKeyboard } from "../hooks/useKeyboard.js";
+import { useSphere } from '@react-three/cannon'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+import { Vector3 } from 'three'
+import { useKeyboard } from '../hooks/useKeyboard.js'
 
-const CHARACTER_SPEED = 4;
-const CHARACTER_JUMP_FORCE = 4;
+const CHARACTER_SPEED = 4
+const CHARACTER_JUMP_FORCE = 4
+
 export const Player = () => {
-  const { moveBackward, moveForward, moveLeft, moveRight, jump } =
-    useKeyboard();
+  const {
+    moveBackward,
+    moveForward,
+    moveLeft,
+    moveRight,
+    jump
+  } = useKeyboard()
 
-  const { camera } = useThree();
+  const { camera } = useThree()
   const [ref, api] = useSphere(() => ({
     mass: 1,
-    type: "Dynamic",
-    position: [0, 1, 0],
-  }));
+    type: 'Dynamic',
+    position: [0, 0.5, 0]
+  }))
 
-  const pos = useRef([0, 0, 0]);
+  const pos = useRef([0, 0, 0])
   useEffect(() => {
-    api.position.subscribe((p) => {
-      pos.current = p;
-    });
-  }, [api.position]);
+    api.position.subscribe(p => {
+      pos.current = p
+    })
+  }, [api.position])
 
-  const vel = useRef([0, 0, 0]);
+  const vel = useRef([0, 0, 0])
   useEffect(() => {
-    api.velocity.subscribe((p) => {
-      vel.current = p;
-    });
-  }, [api.velocity]);
+    api.velocity.subscribe(p => {
+      vel.current = p
+    })
+  }, [api.velocity])
+
   useFrame(() => {
     camera.position.copy(
-      new Vector3(pos.current[0], pos.current[1], pos.current[2])
-    );
+      new Vector3(
+        pos.current[0], // x
+        pos.current[1], // y
+        pos.current[2] // z
+      )
+    )
 
-    const direction = new Vector3();
+    const direction = new Vector3()
 
     const frontVector = new Vector3(
       0,
       0,
       (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
-    );
+    )
 
     const sideVector = new Vector3(
       (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
       0,
       0
-    );
+    )
 
     direction
       .subVectors(frontVector, sideVector)
       .normalize()
-      .multiplyScalar(CHARACTER_SPEED)
-      .applyEuler(camera.rotation);
+      .multiplyScalar(CHARACTER_SPEED) // walk: 2, run: 5
+      .applyEuler(camera.rotation)
 
     api.velocity.set(
       direction.x,
       vel.current[1], // ???? saltar.
       direction.z
-    );
+    )
 
     if (jump && Math.abs(vel.current[1]) < 0.05) {
-        api.velocity.set(
-          vel.current[0],
-          CHARACTER_JUMP_FORCE,
-          vel.current[2]
-        )
-      }
-    //api.velocity.set(0, 0, -1);
-  });
+      api.velocity.set(
+        vel.current[0],
+        CHARACTER_JUMP_FORCE,
+        vel.current[2]
+      )
+    }
+  })
 
-  return <mesh ref={ref} />;
-};
+  return (
+    <mesh ref={ref} />
+  )
+}
--- src/hooks/useKeyboard.js
+++ src/hooks/useKeyboard.js
@@ -1,58 +1,69 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const ACTIONS_KEYBOARD_MAP = {
-  KeyW: "moveForward",
-  KeyS: "moveBackward",
-  KeyA: "moveLeft",
-  KeyD: "moveRight",
-  Space: "jump",
-  Digit1: "dirt",
-  Digit2: "grass",
-  Digit3: "glass",
-  Digit4: "wood",
-  Digit5: "log",
+    KeyW: 'moveForward',
+    KeyS: 'moveBackward',
+    KeyA: 'moveLeft',
+    KeyD: 'moveRight',
+    Space: 'jump',
+    Digit1: 'dirt',
+    Digit2: 'grass',
+    Digit3: 'glass',
+    Digit4: 'wood',
+    Digit5: 'log',
 };
+
 export const useKeyboard = () => {
-  function handleEvent(selection, event) {
-    const { code } = event;
-    const action = ACTIONS_KEYBOARD_MAP[code];
-    if (action) {
-      setActions((prev) => ({
-        ...prev,
-        [action]: selection,
-      }));
-    }
-  }
-  const [actions, setActions] = useState({
-    moveForward: false,
-    moveBackward: false,
-    moveLeft: false,
-    moveRight: false,
-    jump: false,
-    dirt: false,
-    grass: false,
-    glass: false,
-    wood: false,
-    log: false,
-  });
+    const [actions, setActions] = useState({
+        moveForward: false,
+        moveBackward: false,
+        moveLeft: false,
+        moveRight: false,
+        jump: false,
+        dirt: false,
+        grass: false,
+        glass: false,
+        wood: false,
+        log: false,
+    });
 
-  useEffect(() => {
-    const onKeyPress = (event) => {
-      handleEvent(true, event);
-    };
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            const { code } = event;
+            const action = ACTIONS_KEYBOARD_MAP[code];
 
-    const onKeyUp = (event) => {
-      handleEvent(false, event);
-    };
+            if (action) {
+                // if (actions[action]) return
 
-    document.addEventListener("keydown", onKeyPress);
-    document.addEventListener("keyup", onKeyUp);
+                setActions((prevActions) => ({
+                    ...prevActions,
+                    [action]: true,
+                }));
+            }
+        };
 
-    return () => {
-      document.removeEventListener("keydown", onKeyPress);
-      document.removeEventListener("keyup", onKeyUp);
-    };
-  }, []);
+        const handleKeyUp = (event) => {
+            const { code } = event;
+            const action = ACTIONS_KEYBOARD_MAP[code];
 
-  return actions;
+            if (action) {
+                // if (!actions[action]) return
+
+                setActions((prevActions) => ({
+                    ...prevActions,
+                    [action]: false,
+                }));
+            }
+        };
+
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keyup', handleKeyUp);
+        };
+    }, []);
+
+    return actions;
 };
