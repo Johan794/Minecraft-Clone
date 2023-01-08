fd045d09f89fd17502d49bee30edacce66286c5a
Johan794 <johan.ricardo1104@gmail.com>
2023-01-08 15:21:20 -0500

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Canvas } from '@react-three/fiber'
 import { Sky } from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
 import { Ground } from './components/Ground.jsx'
-import { FPV as Fpv } from './components/FPV.jsx'
+import { FPV } from './components/FPV.jsx'
 import { Player } from './components/Player.jsx'
 import { Cubes } from './components/Cubes.jsx'
 import { TextureSelector } from './components/TextureSelect.jsx'
@@ -13,7 +13,7 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
-        <Fpv />
+        <FPV />
 
         <Physics>
           <Cubes />
